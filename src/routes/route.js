b8a812050fad7336c46b4b16e0585aa7e15e56da
ahmaddzidnii <zdngaming312@gmail.com>
2023-10-20 14:13:04 +0700

    02: Membuat Route Test Koneksi Database

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,26 +1,11 @@
 import express from "express";
-import { prisma } from "../lib/prisma/db.js";
+import { HandleGetCities, HandleGetLocationByPostalCode, HandleGetPostalCode } from "../controller/handler.js";
 
 const router = express.Router();
 
-// Rute GET
-router.get("/kodepos", async (req, res) => {
-  const { kodepos } = req.query;
-  const data = await prisma.kodeposid.findMany({
-    where: {
-      kode_pos: parseInt(kodepos),
-    },
-  });
-  // const data = await prisma.kodeposid.findMany({
-  //   take: 25,
-  // });
-
-  res.json({
-    data,
-  });
-});
-router.get("/home", (req, res) => {
-  res.send("Ini adalah rute home");
-});
+// Endpoint untuk mendapatkan data semua kota/kabupaten
+router.get("/cities", HandleGetCities);
+router.get("/postalcode", HandleGetPostalCode);
+router.get("/location", HandleGetLocationByPostalCode);
 
 export default router;
